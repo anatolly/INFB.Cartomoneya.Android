69d133488cd419d46b675c8183c8a170aa75f8e6
Mikhail Zubov <zubov@hrvaas.com>
2015-05-06 08:03:22 +0300

    Push was Missed

--- misc/BarcodeScaner/app/src/main/java/com/intrafab/barcodescaner/MainActivity.java
+++ misc/BarcodeScaner/app/src/main/java/com/intrafab/barcodescaner/MainActivity.java
@@ -1,9 +1,16 @@
 package com.intrafab.barcodescaner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class MainActivity extends Activity {
@@ -35,5 +42,27 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonOnClick(View v) {
+        // do something when the button is clicked
+        Button button = (Button) v;
+
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
