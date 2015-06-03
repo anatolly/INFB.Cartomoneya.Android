e3a9dedb9057d7b347e4c597dbeee16ee7e8f7ab
Mikhail Zubov <mikhailzubov@mac-elephant.local>
2015-06-03 12:19:10 +0300

    adding missed files

--- app/src/main/java/com/intrafab/cartomoneya/BusinessCardsActivity.java
+++ app/src/main/java/com/intrafab/cartomoneya/BusinessCardsActivity.java
@@ -1,12 +1,19 @@
 package com.intrafab.cartomoneya;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.intrafab.cartomoneya.actions.ActionRequestBizCardsTask;
@@ -21,12 +28,18 @@ import com.intrafab.cartomoneya.utils.Logger;
 import com.telly.groundy.CallbacksManager;
 import com.telly.groundy.Groundy;
 
+import java.io.File;
 import java.util.List;
 
 /**
  * Created by Artemiy Terekhov on 06.05.2015.
  */
 public class BusinessCardsActivity extends BaseActivity implements BizCardAdapter.OnClickListener {
+
+    private final int TAKE_PICTURE = 0;
+    private final int SELECT_FILE = 1;
+
+    private String resultUrl = "result.txt";
 
     public static final String TAG = BusinessCardsActivity.class.getName();
 
@@ -172,6 +185,12 @@ public class BusinessCardsActivity extends BaseActivity implements BizCardAdapte
         } else if (id == R.id.action_add_card) {
             NewCardActivity.launch(this);
             return true;
+        } else if (id == R.id.action_OCR_cam) {
+            captureImageFromCamera();
+            return true;
+        } else if (id == R.id.action_OCR_file) {
+            captureImageFromSdCard();
+            return true;
         } else if (id == R.id.action_sync) {
             PlaceholderBizCardsFragment fragment = getFragment();
             if (fragment == null)
@@ -217,4 +236,80 @@ public class BusinessCardsActivity extends BaseActivity implements BizCardAdapte
     public void onClickItem(BizCard itemShopCard) {
         BusinessCardDetailActivity.launch(this, itemShopCard);
     }
+    public void captureImageFromSdCard() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+
+        startActivityForResult(intent, SELECT_FILE);
+    }
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+
+    private static Uri getOutputMediaFileUri(){
+        return Uri.fromFile(getOutputMediaFile());
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "ABBYY Cloud OCR SDK Demo App");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        // Create a media file name
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "image.jpg" );
+
+        return mediaFile;
+    }
+
+    public void captureImageFromCamera( ) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        Uri fileUri = getOutputMediaFileUri(); // create a file to save the image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != Activity.RESULT_OK)
+            return;
+
+        String imageFilePath = null;
+
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                imageFilePath = getOutputMediaFileUri().getPath();
+                break;
+            case SELECT_FILE: {
+                Uri imageUri = data.getData();
+
+                String[] projection = { MediaStore.Images.Media.DATA };
+                Cursor cur = managedQuery(imageUri, projection, null, null, null);
+                cur.moveToFirst();
+                imageFilePath = cur.getString(cur.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            break;
+        }
+
+        //Remove output file
+        deleteFile(resultUrl);
+
+        Intent results = new Intent( this, ResultsActivity.class);
+        results.putExtra("IMAGE_PATH", imageFilePath);
+        results.putExtra("RESULT_PATH", resultUrl);
+        startActivity(results);
+    }
+
 }
