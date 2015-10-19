7f33135e8c07a6f5089d5cad5042ae9002102a4e
Mikhail Zubov <mzubov@hse.ru>
2015-10-19 16:25:08 +0300

    NFC code added

--- app/src/main/java/com/intrafab/cartomoneya/http/RestApiConfig.java
+++ app/src/main/java/com/intrafab/cartomoneya/http/RestApiConfig.java
@@ -8,7 +8,7 @@ import com.intrafab.cartomoneya.Constants;
  */
 public class RestApiConfig {
 
-    public static final String BASE_HOST_NAME = Constants.RELEASE_MODE ? "caramba-shop.ru:1537/api" : "caramba-shop.ru:1537/api"; //1537 1336
+    public static final String BASE_HOST_NAME = Constants.RELEASE_MODE ? "medicus.caramba-shop.ru:1537/api" : "medicus.caramba-shop.ru:1537/api"; //1537 1336
     public static final String VERSION_API = "v1.0";
     public static final String BASE_HOST_URL = "http://" + BASE_HOST_NAME + "/" + VERSION_API;
 
